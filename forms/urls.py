ce7e27298fbb90f081068bb18fdd132cbdcb55e4
tanaymitkari1 <34096328+tanaymitkari1@users.noreply.github.com>
2021-02-10 15:44:19 +0530

    Update urls.py

--- forms/urls.py
+++ forms/urls.py
@@ -4,4 +4,4 @@ from .views import *
 urlpatterns = [
     path('', input_form, name="pdf_form"),
     path('get_data/', export_users_csv, name="data_export"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
